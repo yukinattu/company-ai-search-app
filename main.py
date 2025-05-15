f93658237a5a0f9dadd9370ba155042326028b06
yukinattu <phononeue@gmail.com>
2025-05-15 13:37:14 +0900

    test: enable initialize_session_state

--- main.py
+++ main.py
@@ -45,11 +45,7 @@ try:
     #初期化処理（「initialize.py」の「initialize」関数を実行）
     initialize()
 except Exception as e:    
-    #エラーログの出力
-    logger.error(f"{ct.INITIALIZE_ERROR_MESSAGE}\n{e}")
-    #エラーメッセージの画面表示
-    st.error(utils.build_error_message(ct.INITIALIZE_ERROR_MESSAGE), icon=ct.ERROR_ICON)
-    #後続の処理を中断
+    st.error(f"❌ 初期化エラーの詳細: {e}", icon="🚨")
     st.stop()
 
 #👇 最小構成でStreamlitが動くか確認
